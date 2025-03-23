25b4906b91cc180f2186da508f1f234453e0d50b
yukiofyume <853036144@qq.com>
2025-03-23 18:48:44 +0800

    ollama ds commit

---
+++ src/main/java/com/lwh/learning/springai/SpringAIApplication.java
@@ -0,0 +1,17 @@
+package com.lwh.learning.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lwh
+ * @date 2025-03-20 21:04:14
+ * @describe -
+ */
+@SpringBootApplication
+public class SpringAIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAIApplication.class, args);
+    }
+}
---
+++ src/main/java/com/lwh/learning/springai/config/Myconfig.java
@@ -0,0 +1,19 @@
+//package com.lwh.learning.springai.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.client.SimpleClientHttpRequestFactory;
+//import org.springframework.web.client.RestClient;
+//
+///**
+// * @author lwh
+// * @date 2025-03-20 23:26:08
+// * @describe -
+// */
+//@Configuration
+//public class Myconfig {
+//    @Bean
+//    public RestClient.Builder builder() {
+//        return RestClient.builder().requestFactory(new SimpleClientHttpRequestFactory());
+//    }
+//}
